00e8bc5db28b116e917361dd503ed2491c3c1354
kaye <kaye.lee@luxrobo.com>
2023-06-07 14:12:33 +0900

    fix: 템플릿 메소드 패턴 예제코드 개선

--- behavioral/template_method/01_before/ldap_auth.go
+++ behavioral/template_method/01_before/ldap_auth.go
@@ -9,11 +9,18 @@ func (ldapa *LDAPAuth) Authenticate(id, password string) (string, error) {
 	// step1. 사용자 정보로 인증 확인
 	userName := ldapLogin(id, password)
 
-	fmt.Println("step2. 인증 실패시 예외처리")
-
-	fmt.Println("step3. 인증 성공시 인증정보 제공")
+	ldapa.handleFailedAuthentication()
+	ldapa.handleSuccessAuthentication()
 
 	return userName, nil
+}
+
+func (ldapa *LDAPAuth) handleSuccessAuthentication() {
+	fmt.Println("step3. 인증 성공시 인증정보 제공")
+}
+
+func (ldapa *LDAPAuth) handleFailedAuthentication() {
+	fmt.Println("step2. 인증 실패시 예외처리")
 }
 
 func ldapLogin(id, password string) string {
--- behavioral/template_method/02_after/authenticate/auth.go
+++ behavioral/template_method/02_after/authenticate/auth.go
@@ -20,9 +20,16 @@ func NewAbstractAuth(auth AuthInterface) *Auth {
 func (a *Auth) Auth(id, password string) (string, error) {
 	userName := a.authenticate(id, password)
 
-	fmt.Println("step2. 인증 실패시 예외처리")
-
-	fmt.Println("step3. 인증 성공시 인증정보 제공")
+	a.handleFailedAuthentication()
+	a.handleSuccessAuthentication()
 
 	return userName, nil
+}
+
+func (a *Auth) handleSuccessAuthentication() {
+	fmt.Println("step3. 인증 성공시 인증정보 제공")
+}
+
+func (a *Auth) handleFailedAuthentication() {
+	fmt.Println("step2. 인증 실패시 예외처리")
 }
